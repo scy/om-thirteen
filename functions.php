d67c4ef6f9e5243209177cc932d0c34767aceccb
Tim Weber <scy@scytale.name>
2013-03-30 19:01:32 +0100

    Add normalize.css v1.1.0

--- functions.php
+++ functions.php
@@ -8,6 +8,10 @@ function om13_setup() {
 add_action('after_setup_theme', 'om13_setup');
 
 function om13_enqueue() {
+	wp_enqueue_style(
+		'om13-normalize',
+		get_template_directory_uri() . '/css/normalize-1.1.0.css'
+	);
 	wp_enqueue_style('om13-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'om13_enqueue');
