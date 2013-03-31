dc986b23c9850aeb63a7eb287906309f3d0de80e
Tim Weber <scy@scytale.name>
2013-04-01 00:03:33 +0200

    Release 0.0.1

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 		<?php get_template_part('content', get_post_format()); ?>
 		<hr />
 	<?php endwhile; ?>
+	<?php if (is_page() || is_home()): ?>
+		<?php // TODO: Display a random quote. ?>
+	<?php endif; ?>
 
 <?php else: ?>
 
