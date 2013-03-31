e16b9ce73834f9a5f3797a065b5d515073f8925d
Tim Weber <scy@scytale.name>
2013-03-31 19:41:42 +0200

    Styling for posts with format=quote

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php if (have_posts()): ?>
 
 	<?php while (have_posts()): the_post(); ?>
+		<?php // TODO: Skip posts with format=quote. ?>
 		<?php get_template_part('content', get_post_format()); ?>
 		<hr />
 	<?php endwhile; ?>
