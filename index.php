ca86df1ec64cb1d315b2e52376437c736c7fc81d
Tim Weber <scy@scytale.name>
2013-04-01 09:35:04 +0200

    Show a random quote on the page footer

--- index.php
+++ index.php
@@ -7,7 +7,25 @@
 		<hr />
 	<?php endwhile; ?>
 	<?php if (is_page() || is_home()): ?>
-		<?php // TODO: Display a random quote. ?>
+		<?php
+			$quotes = new WP_Query(array(
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'post_format',
+						'field'    => 'slug',
+						'terms'    => array('post-format-quote'),
+					),
+				),
+				'posts_per_page' => 1,
+				'orderby' => 'rand',
+				'skip_om13_filter_posts' => true,
+			));
+			if ($quotes->have_posts()) {
+				$quotes->the_post();
+				get_template_part('content', get_post_format());
+				wp_reset_postdata();
+			}
+		?>
 	<?php endif; ?>
 
 <?php else: ?>
