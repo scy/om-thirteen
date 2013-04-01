303d5528e311976e3e80d677df0e82a166d18db1
Tim Weber <scy@scytale.name>
2013-04-01 18:37:49 +0200

    Preliminary entry footer

--- content.php
+++ content.php
@@ -5,4 +5,22 @@
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div>
+	<footer class="entry-footer">
+		<?php
+			$date = sprintf('<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a>',
+				esc_url(get_permalink()),
+				esc_attr(get_the_time()),
+				esc_attr(get_the_date('c')),
+				esc_html(get_the_date())
+			);
+			$author = sprintf('%1$s',
+				get_the_author()
+			);
+			$wrapped = array();
+			foreach (array($date, $author) as $item) {
+				$wrapped[] = "<span class=\"meta-piece\">$item</span>";
+			}
+			echo implode(' ', $wrapped);
+		?>
+	</footer>
 </article>
