a6b6d776f7aafd1c9817b5b22751f8d0450372b5
Tim Weber <scy@scytale.name>
2013-04-07 19:36:06 +0200

    Add a social media footer

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 </div><!-- #main -->
 
-<footer>
+<hr />
+<footer class="page-footer">
+<a href="https://twitter.com/openmindkonf">Twitter</a> | 
+<a href="https://facebook.com/openmind.konferenz">Facebook</a> | 
+<a href="https://plus.google.com/118100230661845993722" rel="publisher">Google+</a>
 </footer>
 
 </div><!-- #page -->
